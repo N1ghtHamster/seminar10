2cd78a98dba4c5502ec0cb9a09c8a10bcb6b0130
Pavel Priklonskiy <votymu@yandex.ru>
2023-11-28 01:42:24 +0300

    Update 10_3

--- 10_3/func.c
+++ 10_3/func.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
 void change_path(char *str, char *dst)
 {
-	while (*str != '\0')
-		printf("%c", *str++);
+	int lenstr = strlen(str);
+	int lentmp = 0, lenend = 0;
+	int position = 0;
+	char *tmp = NULL, *end = NULL;
+	tmp = strrchr(str, '/');
+	while (*tmp++ != '\0'){lentmp++;}
+	tmp = strrchr(str, '/');
+	end = strrchr(tmp, '.');
+	if (end != NULL && *(end + 1) != '\0')
+		while (*end++ != '\0'){lenend++;}
+	end = strrchr(tmp, '.');
+	if (end != NULL && *(end + 1) == '\0')
+		position = lenstr - 1;
+	else if (tmp != NULL && end == NULL)
+		position = lenstr;
+	else if (tmp != NULL && end != NULL)
+		position = lenstr - lenend;
+	else
+		position = lenstr - lentmp - lenend;
+	strncpy(dst, str, position);
+	strcat(dst, ".html\0");
 }
